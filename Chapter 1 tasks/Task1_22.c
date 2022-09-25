f036449a1e0e81d1fcb19c000c254564330531cf
Adedeji88 <hello@mustaphadedeji@yahoo.com>
2022-09-25 13:00:19 +0100

    New commit

--- Chapter 1 tasks/Task1_22.c
+++ Chapter 1 tasks/Task1_22.c
@@ -5,8 +5,8 @@ int main(void)
     float area, vol, r, h; /* r and h are radius and height of cylinder */
     float pi = 3.14159;
 
-    printf("Enter the value of r: ");
-    scanf("%f", &r);
+    printf("Enter the values of r and h: ");
+    scanf("%f %f", &r, &h);
 
     area = pi * r * r;
     vol = area * h;
