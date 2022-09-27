ae02f2be8ed0666231ed2436a66aa745ec2b374e
Adedeji88 <hello@mustaphadedeji@yahoo.com>
2022-09-27 17:46:21 +0100

    task_3-6

--- Chapter 3 tasks/task3-6.c
+++ Chapter 3 tasks/task3-6.c
@@ -10,19 +10,19 @@ int main(void)
     printf("Enter the values of num1, num2, num3, and num4: ");
     scanf("%d %d %d %d", &num1, &num2, &num3, &num4);
 
-    if((num1 > num2) && (num2 > num3) && (num3 > num4))
+    if((num1 > num2) && (num1 > num3) && (num1 > num4))
     {
         printf("num1 is the greatest");
     }
-    else if((num2 > num1) && (num1 > num3) && (num3 > num4))
+    else if((num2 > num1) && (num2 > num3) && (num2 > num4))
     {
         printf("num2 is the greatest");
     }
-    else if((num3 > num1) && (num1 > num2) && (num2 > num4))
+    else if((num3 > num1) && (num3 > num2) && (num3 > num4))
     {
         printf("num3 is the greatest");
     }
-    else if((num4 > num1) && (num1 > num2) && (num2 > num3))
+    else if((num4 > num1) && (num4 > num2) && (num4 > num3))
     {
         printf("num4 is the greatest");
     }
