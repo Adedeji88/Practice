217d0344f81a306458a56153fd16788ac2eafb09
Adedeji88 <hello@mustaphadedeji@yahoo.com>
2022-10-01 14:13:33 +0100

    New task

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,31 +1,14 @@
 #include <stdio.h>
+int main(void)
+{
+    int i, j, prod;
 
-int main() {
-
-  int n, i, flag = 0;
-  printf("Enter a positive integer: ");
-  scanf("%d", &n);
-
-  // 0 and 1 are not prime numbers
-  // change flag to 1 for non-prime number
-  if (n == 0 || n == 1)
-    flag = 1;
-
-  for (i = 2; i <= n / 2; ++i) {
-
-    // if n is divisible by i, then n is not prime
-    // change flag to 1 for non-prime number
-    if (n % i == 0) {
-      flag = 1;
-      break;
+    for(i = 1; i <= 10; i++)
+    {
+        for(j = 10; j <= 10; j--)
+        {
+            prod = i * j;
+        }
+        printf("%d", prod);
     }
-  }
-
-  // flag is 0 for prime numbers
-  if (flag == 0)
-    printf("%d is a prime number.", n);
-  else
-    printf("%d is not a prime number.", n);
-
-  return 0;
 }
