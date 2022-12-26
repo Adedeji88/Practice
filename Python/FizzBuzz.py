e0771cf48701ee9e36bd28b125f037b1186af304
Adedeji88 <mustaphadedeji@yahoo.com>
2022-12-26 08:13:44 +0100

    Wrote FizzBuzz code

---
+++ Python/FizzBuzz.py
@@ -0,0 +1,11 @@
+n = int(input("Enter any number between 1 and n excluding n: "))
+
+for i in range(1, n):
+    if i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    elif i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    else:
+        print(i)
